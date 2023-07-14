2119058c42d6bf2fa525a6d255759386e9b1dde7
pachieng <pachieng19@gmail.com>
2023-07-14 15:27:15 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,11 +19,11 @@ int main(void)
 				putchar(p);
 				if (d == '8' && p == '9')
 					continue;
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
